2c4c707f9f72d0a62eecf303e82492ada6d5037e
James Cox <james@imaj.es>
2023-04-28 12:13:57 -0500

    switch to clap for subcommand structuring

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,85 @@
-extern crate getopts;
-use getopts::Options;
-use std::env;
+use std::path::PathBuf;
 
-mod ergast_client;
+use clap::{Parser, Subcommand};
 
-fn get_race_data() {
-    let race_data = match ergast_client::get_season_data(2022) {
-        Ok(rv) => rv,
-        Err(e) => panic!("Problem doing the thing: {:?}", e),
-    };
+// mod ergast_client;
 
-    println!("{:#?}", race_data[0]);
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    verbose: bool,
+
+    /// Don't do anything, just report
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    dryrun: bool,
 }
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options] METHOD", program);
-    print!("{}", opts.usage(&brief));
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// generates folders in the destination output
+    Generate {
+        /// dest path to move files to
+        #[arg(short, long, value_name="PATH")]
+        dest: Option<PathBuf>,
+    },
+
+    Relocate {
+        /// source path to move files from
+        #[arg(short, long, value_name="PATH")]
+        src: Option<PathBuf>,
+
+        /// dest path to move files to
+        #[arg(short, long, value_name="PATH")]
+        dest: Option<PathBuf>,
+    },
+
+    Watch {
+        /// source path to move files from
+        #[arg(short, long, value_name="PATH")]
+        watch: Option<PathBuf>,
+
+        /// dest path to move files to
+        #[arg(short, long, value_name="PATH")]
+        dest: Option<PathBuf>,
+    },
 }
+
+// fn get_race_data() {
+//     let race_data = match ergast_client::get_season_data(2022) {
+//         Ok(rv) => rv,
+//         Err(e) => panic!("Problem doing the thing: {:#?}", e),
+//     };
+//
+//     println!("{:#?}", race_data[0]);
+// }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
 
-    let mut opts = Options::new();
+    let cli = Cli::parse();
+    println!("cli.command: {:#?}", cli.command);
 
-    // mode/method [create folders, move files, observe new files]
+    let subcommand = cli.command.as_ref();
 
-    opts.optflag("f", "folders", "Create the hierarchy of folders for plex");
-    opts.optflag("m", "move", "Move the --src files into the --dest folders");
-    opts.optflag("w", "watch", "Watch a folder for incoming downloads and put them in the correct place");
+    // if subcommand == None {
+    //     println!("Help requested");
+    //     return;
+    // }
+    //
+    println!("subcommand is {:#?}", subcommand);
 
-    opts.optopt("c", "create", "Path to create folders in", "PATH");
-    opts.optopt("s", "src", "Path to search/source files from", "PATH");
-    opts.optopt("d", "dest", "Path to move files to", "PATH");
-
-    opts.optflag("", "debug", "Print debugging messages");
-    opts.optflag("n", "dry-run", "Don't execute any changes, just report");
-    opts.optflag("h", "help", "print this help menu");
-
-    // parse matches from opt
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => { m }
-        Err(f) => { panic!("{:#?}", f.to_string()) }
-    };
-
-    if matches.opt_present("d") {
-        // debugging on
+    match subcommand {
+        Some(Generate) => println!("This calls the generate sub option command"),
+        Some(Relocate) => println!("This calls the relocate sub option command"),
+        Some(Watch) => println!("This calls the watch sub option command"),
+        None => println!("None - Shouldn't happen here"),
+        _ => panic!("Unrecognized command requested: {:#?}", subcommand),
     }
 
-    if matches.opt_present("n") {
-        // dry run present
-    }
-
-    if matches.opt_present("h") {
-        // print help/usage
-        print_usage(&program, opts);
-        return;
-    }
-
-    // track paths
-    let _dest = matches.opt_str("d");
-    let _src = matches.opt_str("s");
-    let _create = matches.opt_str("c");
-
-    println!("{:#?}", matches.free);
-
-    let _input = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
-        print_usage(&program, opts);
-        return;
-    };
-
-    get_race_data();
+    // get_race_data();
 }
